bb237122bdf22a269fbf3f5b83906a9a28ed209d
abhinav0411 <abhinavrai0419@gmail.com>
2025-03-11 01:16:50 +0530

    Added pygame screen and some roms

--- emulator.py
+++ emulator.py
@@ -1,9 +1,10 @@
-#Emulator for a CHIP-8 system
+#Emulator for a CHIP-8 system using Python
 
 
 #Importing all required modules
 
 import random
+import pygame
 
 #Making the class for the structure of emulator
 class Chip8:
@@ -16,12 +17,62 @@ class Chip8:
         self.delay_timer = 0
         self.sound_timer = 0
         self.keypad = [0] * 16
-        self.display = [[0] * 64 for i in range(32)]
+        self.display = [[0] * 64 for _ in range(32)]
+
+    
+    #Function for timers
+    def update_timer(self):
+        if (self.delay_timer > 0):
+            self.delay_timer -= 1
+        
+        if (self.sound_timer > 0):
+            self.sound_timer -=1
+
+    #Function for loading fonts
+    def load_fonts(self):
+        FONT_SET = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0,  
+            0x20, 0x60, 0x20, 0x20, 0x70,  
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,  
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,  
+            0x90, 0x90, 0xF0, 0x10, 0x10,  
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,  
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,  
+            0xF0, 0x10, 0x20, 0x40, 0x40,  
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,  
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, 
+            0xF0, 0x90, 0xF0, 0x90, 0x90, 
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,  
+            0xF0, 0x80, 0x80, 0x80, 0xF0,  
+            0xE0, 0x90, 0x90, 0x90, 0xE0,  
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,  
+            0xF0, 0x80, 0xF0, 0x80, 0x80   
+        ]
+        for i, byte in enumerate(FONT_SET):
+            self.memory[i] = byte
+
+    #Function for drawing
+    def draw(self, screen):
+        screen.fill((0, 0, 0))
+
+        for y in range(32):
+            for x in range(64):
+                if self.display[y][x]:
+                    pygame.draw.rect(screen, (255, 255, 255), 
+                                     (x * 10, y * 10, 10, 10))
+        
+        pygame.display.flip()
 
 
-#Loading ROM into memory
-def load_ROM(chip8, ROM):
-    chip8.memory[0x200:0x200+len(ROM)] = ROM
+    #Function to load ROM files
+    def load_ROM(self, filename):
+        with open(filename, 'rb') as rom:
+            rom_data = rom.read()
+            for i, byte in enumerate(rom_data):
+                self.memory[0x200 + i] = byte
+
+
+
 
 #Implementing Fetch, Decode, Execute   
 def fetch_code(chip8):
@@ -36,11 +87,14 @@ def decode_and_execute(chip8, opcode):
     # nnn = (opcode & 0x0FFF)
     # N = (opcode & 0xF000) >> 12
 
+
     if (opcode == 0x00E0):  
         chip8.display = [[0] * 64 for _ in range(32)]
+        chip8.PC += 2
     
     elif (opcode == 0x00EE):  
         chip8.PC = chip8.stack.pop()
+        chip8.PC += 2 
     
     elif (opcode & 0xF000) == 0x1000:  
         nnn = opcode & 0x0FFF
@@ -56,6 +110,8 @@ def decode_and_execute(chip8, opcode):
         nn = (opcode & 0x00FF) 
         if chip8.V[x] == nn:
             chip8.PC += 2
+        chip8.PC += 2 
+
     elif (opcode & 0xF000) == 0x4000:
         x = (opcode & 0x0F00) >> 8
         nn = (opcode & 0x00FF) 
@@ -76,31 +132,39 @@ def decode_and_execute(chip8, opcode):
         x = (opcode & 0x0F00) >> 8
         nn = (opcode & 0x00FF) 
         chip8.V[x] = nn
+        chip8.PC += 2
+        
 
     elif (opcode & 0xF000) == 0x7000:
         x = (opcode & 0x0F00) >> 8
         nn = (opcode & 0x00FF)
         chip8.V[x] += nn
+        chip8.PC += 2 
 
     elif (opcode & 0xF000) == 0x8000:
         x = (opcode & 0x0F00) >> 8
         y = (opcode & 0x00F0) >> 4
         chip8.V[x] = chip8.V[y]
+        chip8.PC += 2 
+
     elif (opcode & 0xF00F) == 0x8001:
         x = (opcode & 0x0F00) >> 8
         y = (opcode & 0x00F0) >> 4
         chip8.V[x] = chip8.V[x] | chip8.V[y]
+        chip8.PC += 2 
 
     elif (opcode & 0xF00F) == 0x8002:
         x = (opcode & 0x0F00) >> 8
         y = (opcode & 0x00F0) >> 4
         chip8.V[x] = chip8.V[x] & chip8.V[y]
+        chip8.PC += 2
     
     elif (opcode & 0xF00F) == 0x8003:
         x = (opcode & 0x0F00) >> 8
         y = (opcode & 0x00F0) >> 4
         # chip8.V[x] = (chip8.V[x] | chip8.V[y]) & ~(chip8.V[x] & chip8.V[y])  -> alternate way of creating an XOR gate in Python
         chip8.V[x] = chip8.V[x] ^ chip8.V[y]
+        chip8.PC += 2
 
     elif (opcode & 0xF00F) == 0x8004:
         x = (opcode & 0x0F00) >> 8
@@ -112,6 +176,8 @@ def decode_and_execute(chip8, opcode):
             chip8.V[15] = 0
 
         chip8.V[x] = ((chip8.V[x] + chip8.V[y]) & 0xFF)
+
+        chip8.PC += 2 
     
     elif (opcode & 0xF00F) == 0x8005:
         x = (opcode & 0x0F00) >> 8
@@ -123,11 +189,13 @@ def decode_and_execute(chip8, opcode):
             chip8.V[15] = 0
         
         chip8.V[x] = (chip8.V[x] - chip8.V[y]) & 0xFF
+        chip8.PC += 2 
 
     elif (opcode & 0xF00F) == 0x8006:
         x = (opcode & 0x0F00) >> 8
         chip8.V[15] = chip8.V[x] & 0x01
         chip8.V[x] = chip8.V[x] >> 1
+        chip8.PC += 2 
 
     elif (opcode & 0xF00F) == 0x8007:
         x = (opcode & 0x0F00) >> 8
@@ -139,11 +207,13 @@ def decode_and_execute(chip8, opcode):
             chip8.V[15] = 0
 
         chip8.V[x] = (chip8.V[y] - chip8.V[x]) & 0xFF
+        chip8.PC += 2 
     
     elif (opcode & 0xF00F) == 0x800E:
         x = (opcode & 0x0F00) >> 8
         chip8.V[15] = (chip8.V[x] & 0x80) >> 7
         chip8.V[x] = (chip8.V[x] << 1) & 0xFF
+        chip8.PC += 2 
 
     elif (opcode & 0xF00F) == 0x9000:
         x = (opcode & 0x0F00) >> 8
@@ -157,6 +227,7 @@ def decode_and_execute(chip8, opcode):
     elif (opcode & 0xF000) == 0xA000:
         nnn = (opcode & 0x0FFF)
         chip8.I = nnn
+        chip8.PC += 2
 
     elif (opcode & 0xF000) == 0xB000:
         nnn = (opcode & 0x0FFF)
@@ -166,6 +237,7 @@ def decode_and_execute(chip8, opcode):
         x = (opcode & 0x0F00) >> 8
         nn = (opcode & 0x00FF)
         chip8.V[x] = random.randint(0,255) & nn
+        chip8.PC += 2
         
     elif (opcode & 0xF000) == 0xD000:
         x = chip8.V[(opcode & 0x0F00) >> 8] % 64
@@ -185,6 +257,8 @@ def decode_and_execute(chip8, opcode):
 
                     chip8.display[screen_y][screen_x] ^= 1
 
+        chip8.PC += 2
+
     elif (opcode & 0xF0FF) == 0xE09E:
         x = (opcode & 0x0F00) >> 8
         key = chip8.V[x]
@@ -200,13 +274,15 @@ def decode_and_execute(chip8, opcode):
     elif (opcode & 0xF0FF) == 0xF007:
         x = (opcode & 0x0F00) >> 8
         chip8.V[x] = chip8.delay_timer
+        chip8.PC += 2
 
     elif (opcode & 0xF0FF) == 0xF00A:
         x = (opcode & 0x0F00) >> 8
         key_pressed = False  
         for i in range(16):  
             if chip8.keypad[i]:  
-                chip8.V[x] = i  
+                chip8.V[x] = i
+                chip8.PC += 2  
                 key_pressed = True
                 break
         if not key_pressed:
@@ -215,24 +291,30 @@ def decode_and_execute(chip8, opcode):
     elif (opcode & 0xF0FF) == 0xF015:
         x = (opcode & 0x0F00) >> 8
         chip8.delay_timer = chip8.V[x]
+        chip8.PC += 2
 
     elif (opcode & 0xF0FF) == 0xF018:
         x = (opcode & 0x0F00) >> 8
         chip8.sound_timer = chip8.V[x]
+        chip8.PC += 2
     
     elif (opcode & 0xF0FF) == 0xF01E:
         x = (opcode & 0x0F00) >> 8
-        chip8.I = chip8.V[x]
+        chip8.I += chip8.V[x]
+        chip8.PC += 2
 
     elif (opcode & 0xF0FF) == 0xF029:
         x = (opcode & 0x0F00) >> 8
-        chip8.I = chip8.V[x] * 5
+        digit = chip8.V[x] & 0xF
+        chip8.I = digit * 5
+        chip8.PC += 2
 
     elif (opcode & 0xF0FF) == 0xF033:
         x = (opcode & 0x0F00) >> 8
         chip8.memory[chip8.I] = (int(chip8.V[x]) // 100)
         chip8.memory[chip8.I+1] = (int(chip8.V[x]) % 100) // 10
         chip8.memory[chip8.I+2] = (int(chip8.V[x]) % 10)
+        chip8.PC += 2
 
     elif (opcode & 0xF0FF) == 0xF055:
         x = (opcode & 0x0F00) >> 8
@@ -245,4 +327,5 @@ def decode_and_execute(chip8, opcode):
 
         for i in range(x+1):
             chip8.V[i] = chip8.memory[chip8.I + i]
+    
         
---
+++ screen.py
@@ -0,0 +1,59 @@
+#Emulator for a CHIP-8 system using Python
+
+
+#Importing all required modules
+
+import pygame
+import emulator
+
+#Pygame setup
+pygame.init()
+SCALE = 10
+screen = pygame.display.set_mode((64 * SCALE,32 * SCALE))
+clock = pygame.time.Clock()
+running = True
+chip8 = emulator.Chip8()
+
+#Loading the ROM file
+try:
+    chip8.load_ROM("TETRIS.ch8")
+except FileNotFoundError:
+    print("File not found")
+    running = False
+
+
+#Keymap for chip8
+keymap = {
+    pygame.K_1: 0x1, pygame.K_2: 0x2, pygame.K_3: 0x3, pygame.K_4: 0xC,
+    pygame.K_q: 0x4, pygame.K_w: 0x5, pygame.K_e: 0x6, pygame.K_r: 0xD,
+    pygame.K_a: 0x7, pygame.K_s: 0x8, pygame.K_d: 0x9, pygame.K_f: 0xE,
+    pygame.K_z: 0xA, pygame.K_x: 0x0, pygame.K_c: 0xB, pygame.K_v: 0xF,
+}
+
+#Loop for running
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key in keymap:
+                chip8.keypad[keymap[event.key]] = 1
+        
+        elif event.type == pygame.KEYUP:
+            if event.key in keymap:
+                chip8.keypad[keymap[event.key]] = 0
+    
+    
+
+    opcode = emulator.fetch_code(chip8)
+    emulator.decode_and_execute(chip8, opcode)
+
+
+    chip8.update_timer()
+
+    
+    chip8.draw(screen)
+    clock.tick(60)
+
+pygame.quit()
